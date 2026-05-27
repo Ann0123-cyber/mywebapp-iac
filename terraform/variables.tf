a75274881b3dca19516ffb0f81d34261db245a85
Ann0123-cyber <anna.musiiovska@gmail.com>
2026-05-27 06:11:01 +0300

    fix: correct ansible public key_2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,5 +5,5 @@ variable "base_image_path" {
 
 variable "ansible_public_key" {
   description = "SSH public key for ansible user"
-  default     = "AAAAC3NzaC1lZDI1NTE5AAAAIMA/GqYtmjT8INwMTlu8dUPyyPK4+0KLbbTikD0RD6KX"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMA/GqYtmjT8INwMTlu8dUPyyPK4+0KLbbTikD0RD6KX"
 }
